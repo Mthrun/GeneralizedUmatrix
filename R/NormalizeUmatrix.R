90b35ad55207e7acd7ddede2034101fc0a405b58
Mthrun <m.thrun@gmx.net>
2019-12-08 10:05:23 +0100

    normalizing umatrix with abstract U-Matrix

--- R/NormalizeUmatrix.R
+++ R/NormalizeUmatrix.R
@@ -152,7 +152,106 @@ getAbstractUMatrix <- function(BestMatches, Size, Data, BorderAlg = BorderToGrid
   #
   # AUTHOR
   # Felix Pape 11/2016
-  
+  uniquePoints <- function(data,mindist = 1e-10){
+    # U <- uniquePoints(data)
+    # return only the unique points in data
+    #
+    # INPUT
+    # data[1:n,1:d]   The vector/matrix with n data points of dimension d
+    #				          the points are in the  rows
+    # mindist					everything with less distance is considered equal
+    #
+    # OUTPUT
+    # a list U containg:
+    # UniqueData  <- U$unique[1:u,1:d]       the data points  without duplicate points
+    # UniqueInd   <- U$sortind[1:u]		       an index vector such that unique ==  data[sortind,]
+    # Uniq2DataInd<- U$mergeind[1:n] 	       an index vector such that   data ==  unique[mergeind,]
+    # IsDuplicate <- U$IsDuplicate[1:n,1:n]  for i!=j IsDuplicate[i,j]== 1  iff data[i,] == data[j,] ;  IsDuplicate[i,i]==0
+    #
+    # Complete Code rewrite by FP 03/17 
+    # 1.Editor: MT 03/17: mindist als Parameter eingefuehrt
+    
+    
+    # when data is a vector, convert to matrix
+    if (class(data) == "numeric" || class(data) == "complex") {
+      data <- matrix(data, ncol = 1)
+    } else if (class(data) == "data.frame") {
+      data <- as.matrix(data)
+    } else if (class(data) != "matrix") {
+      stop("uniquePoints input is neither a (numeric or complex) vector, matrix or data.frame.")
+    }
+    
+    NumData = nrow(data)
+    
+    distsmat = as.matrix(dist(data))
+    # * 1 is to "hack" TRUE to 1 and FALSE to 0
+    IsDuplicate = ((distsmat) < mindist) * 1 - diag(NumData)
+    
+    # Hack: set distance of every node to itself to NA (we're not interested in these distances)
+    # otherwise every element would be a duplicate of itself later.
+    distsmat = distsmat + diag(rep(NA, nrow(distsmat)))
+    
+    # No duplicates found
+    if (length(which((distsmat) < mindist)) == 0) {
+      return(list(
+        "unique" = unique(data),
+        "sortind" = c(1:NumData),
+        "mergeind" = c(1:NumData),
+        IsDuplicate = IsDuplicate
+      ))
+    }
+    
+    # save rownames
+    origrows = rownames(data)
+    # use rownames to save original index
+    rownames(data) = c(1:NumData)
+    
+    #  find out which distances are smaller than mindist (and as such duplicates)
+    # (except the diagonal, so we artificially increase it)
+    dups = which((distsmat) < mindist, arr.ind = T)
+    
+    # indicies of rows which will be deleted
+    delinds = c()
+    while (dim(dups)[1] > 0) {
+      ind = dups[1, 1]
+      delinds = c(delinds, ind)
+      # remove every row in which the duplicate also occurs.
+      # this is to prevent to also delete the "original"
+      # if not save, encapuslate right hand side in matrix( ... , nrow = 2)
+      dups = dups[-(which(dups == ind, arr.ind = T)[, 1]), ]
+    }
+    # delete duplicates, stay as matrix (important for vector input) and keep rownames
+    uniquedata = data[-delinds, ]
+    uniquedata = matrix(uniquedata, ncol = ncol(data))
+    rownames(uniquedata) = rownames(data)[-delinds]
+    
+    # the rownames contain the indicies in the original data
+    sortind = as.numeric(rownames(uniquedata))
+    
+    # calculate the mergind. At the end there should not be a NA in mergeind left
+    mergeind = rep(NA, NumData)
+    mergeind[sortind] = 1:nrow(uniquedata)
+    # this works due to the fact that:
+    #   union(sortind, delinds) == 1:NumData (if everything is sorted)
+    for (i in delinds) {
+      candidates = which(IsDuplicate[i, ] == 1)
+      original = intersect(candidates, sortind)
+      mergeind[i] = which(sortind == original)
+    }
+    
+    # restore rownames of the original data
+    rownames(uniquedata) = origrows[-delinds]
+    
+    return(
+      list(
+        "unique" = as.matrix(uniquedata),
+        "sortind" = sortind,
+        "mergeind" = mergeind,
+        IsDuplicate = IsDuplicate
+      )
+    )
+    
+  } #end function uniquePoints
   if(ncol(BestMatches) != 3)
     stop("BestMatches must be in the usual format. 3 columns: ID, Line, Column")
   if(!is.vector(Size) || length(Size) != 2)
@@ -228,8 +327,75 @@ NormalizeUmatrix = function(Data, Umatrix, BestMatches) {
   }
   if (b[1] != nrow(Data))
     stop('Number of Data cases do not equal Number of BestMatches')
-  aumx = getAbstractUMatrix(BestMatches, Size = size(Umatrix), Data = Data)
+  aumx = getAbstractUMatrix(BestMatches, Size = dim(Umatrix), Data = Data)
   normalized = normUmatrix(aumx, Umatrix)
   
   return(normalized)
 }
+
+meanrobust <- function(x, p=0.1){
+  
+  if(is.matrix(x)){
+    mhat<-c()
+    for(i in 1:dim(x)[2]){
+      mhat[i]<-mean(x[,i],trim=p,na.rm=TRUE)
+    }
+  } else  mhat<-mean(x,trim=p,na.rm=TRUE) 
+  
+  return (mhat) 
+  
+}
+
+stdrobust <- function(x,lowInnerPercentile=25){
+  
+  if(is.vector(x) || (is.matrix(x) && dim(x)[1]==1)) dim(x)<-c(length(x),1)
+  
+  lowInnerPercentile<-max(1,min(lowInnerPercentile,49))
+  hiInnerPercentile<- 100 - lowInnerPercentile
+  #norminv=qnorm
+  faktor<-sum(abs(qnorm(c(lowInnerPercentile,hiInnerPercentile)/100,0,1)))
+  std<-sd(x,na.rm=TRUE)
+  
+  quartile<-prctile(x,c(lowInnerPercentile,hiInnerPercentile))  
+  if (ncol(x)>1)
+    iqr<-quartile[2,]-quartile[1,]
+  else
+    iqr<-quartile[2]-quartile[1]
+  
+  shat<-c()
+  for(i in 1:ncol(x)){
+    shat[i]<-min(c(std[i],iqr[i]/faktor),na.rm=TRUE)
+  }
+  dim(shat)<-c(1,ncol(x))
+  colnames(shat)<-colnames(x)
+  return (shat) 
+  
+}
+
+prctile<-function(x,p){
+  #   matlab:
+  #   Y = prctile(X,p) returns percentiles of the values in X. 
+  #   p is a scalar or a vector of percent values. When X is a 
+  #   vector, Y is the same size as p and Y(i) contains the p(i)th 
+  #   percentile. When X is a matrix, the ith row of Y contains the 
+  #   p(i)th percentiles of each column of X. For N-dimensional arrays,
+  #   prctile operates along the first nonsingleton dimension of X.  
+  if(length(p)==1){  
+    if(p>1){p=p/100}
+    
+  }
+  if(max(p)>1)
+    p=p/100
+  
+  if(is.matrix(x) && ncol(x)>1){
+    cols<-ncol(x)
+    quants<-matrix(0,nrow=length(p),ncol=cols)
+    for(i in 1:cols){
+      quants[,i]<-quantile(x[,i],probs=p,type=5,na.rm=TRUE)
+    }
+  }else{
+    quants<-quantile(x,p,type=5,na.rm=TRUE)
+  }
+  return(quants)
+}
+
